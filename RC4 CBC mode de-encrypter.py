1c6738a5ce07bfe7efdd5ab1e44c472f6bb62cb3
Edvinas Narbutas <enarbutas@gmail.com>
2012-07-01 13:38:09 +0300

    Refactored method names and field names

--- RC4 CBC mode de-encrypter.py
+++ RC4 CBC mode de-encrypter.py
@@ -4,204 +4,206 @@ import sys
 import time
 
 class RC4_CBC():
-    def encRC4_CBC(self, tekstas, baitai, raktas):
-        tekstas_baitais = self.plain_to_baitai(tekstas)
-        key_stream = self.getKSA(raktas, baitai)
-        key_stream = self.getPRGA(key_stream, len(tekstas), baitai)
-        uzkuoduotas_tekstas = self.tekstas_baitais_XOR_keystream(tekstas_baitais, key_stream)
-        uzkuoduotas_tekstas = self.baitai_to_hex(uzkuoduotas_tekstas)
-        uzkuoduotas_tekstas = uzkuoduotas_tekstas.toUpper()
-        return uzkuoduotas_tekstas
+    def encRC4_CBC(self, text, bytes, key):
+        text_in_bytes = self.plain_to_bytes(text)
+        key_stream = self.getKSA(key, bytes)
+        key_stream = self.getPRGA(key_stream, len(text), bytes)
+        encrypted_text = self.text_in_bytes_XOR_keystream(text_in_bytes, key_stream)
+        encrypted_text = self.bytes_to_HEX(encrypted_text)
+        encrypted_text = encrypted_text.toUpper()
+        return encrypted_text 
     
-    def decRC4_CBC(self, tekstas, baitai, raktas):
-        tekstas_baitais = self.hex_to_baitai(str(tekstas))
-        key_stream = self.getKSA(raktas, baitai)
-        key_stream = self.getPRGA(key_stream, len(tekstas_baitais), baitai)
-        dekuoduotas_tekstas = self.tekstas_baitais_XOR_keystream(tekstas_baitais, key_stream)
-        dekuoduotas_tekstas = self.baitai_to_plain(dekuoduotas_tekstas)
-        return dekuoduotas_tekstas
+    def decRC4_CBC(self, text, bytes, key):
+        text_in_bytes = self.HEX_to_bytes(str(text))
+        key_stream = self.getKSA(key, bytes)
+        key_stream = self.getPRGA(key_stream, len(text_in_bytes), bytes)
+        encrypted_text = self.text_in_bytes_XOR_keystream(text_in_bytes, key_stream)
+        encrypted_text = self.bytes_to_plain(encrypted_text)
+        return encrypted_text
     
-    def getKSA(self, raktas, baitais):
+    def getKSA(self, key, bytes):
         j = 0
-        KSA_list = [i for i in range(baitais)]
-        for i in range(baitais):
-            j = (j + KSA_list[i] + ord(raktas[i % len(str(raktas))])) % baitais
+        KSA_list = [i for i in range(bytes)]
+        for i in range(bytes):
+            j = (j + KSA_list[i] + ord(key[i % len(str(key))])) % bytes
             swap = KSA_list[i]
             KSA_list[i] = KSA_list[j]
             KSA_list[j] = swap
         return KSA_list
     
-    def getPRGA(self, keyStream, size, baitai):
+    def getPRGA(self, key_stream, size, bytes):
         i = 0
         j = 0
         prga = []
         for i in range(size):
-            i = (i + 1) % baitai
-            j = (j + keyStream[i]) % baitai
-            swap = keyStream[i]
-            keyStream[i] = keyStream[j]
-            keyStream[j] = swap
-            prga.append(keyStream[(keyStream[i] + keyStream[j]) % baitai])
+            i = (i + 1) % bytes
+            j = (j + key_stream[i]) % bytes
+            swap = key_stream[i]
+            key_stream[i] = key_stream[j]
+            key_stream[j] = swap
+            prga.append(key_stream[(key_stream[i] + key_stream[j]) % bytes])
         return prga
     
-    def tekstas_baitais_XOR_keystream(self, tekstas_baitais, key_stream):
-        uzkuoduotas_tekstas = []
-        for i in range(len(tekstas_baitais)):
-            uzkuoduotas_tekstas.append(tekstas_baitais[i] ^ key_stream[i])
-        return uzkuoduotas_tekstas
+    def text_in_bytes_XOR_keystream(self, text_in_bytes, key_stream):
+        encrypted_text = []
+        for i in range(len(text_in_bytes)):
+            encrypted_text.append(text_in_bytes[i] ^ key_stream[i])
+        return encrypted_text
         
-    def baitai_to_hex(self, baitai):
+    def bytes_to_HEX(self, bytes):
         hex_tektas = ""
-        for byte in baitai:
+        for byte in bytes:
             hex_tektas += QString("%1").arg(byte, 0, 16)
             hex_tektas += " "
         return hex_tektas
     
-    def hex_to_baitai(self, hex_tekstas):
-        tekstas_baitai = []
-        hex_tekstas = hex_tekstas.strip()
-        hex_tekstas = hex_tekstas.split(" ")
-        for hexaD in hex_tekstas:
+    def HEX_to_bytes(self, HEX_text):
+        text_in_bytes = []
+        HEX_text = HEX_text.strip()
+        HEX_text = HEX_text.split(" ")
+        for hexaD in HEX_text:
             hexaD = QString(hexaD)
             hexaD = hexaD.toInt(16)
-            tekstas_baitai.append(hexaD[0])
-        return tekstas_baitai
+            text_in_bytes.append(hexaD[0])
+        return text_in_bytes
     
-    def plain_to_baitai(self, tekstas):
-        baitai = []
-        for char in tekstas:
-            baitai.append(ord(char))
-        return baitai
+    def plain_to_bytes(self, text):
+        bytes = []
+        for char in text:
+            bytes.append(ord(char))
+        return bytes
     
-    def baitai_to_plain(self, baitai):
+    def bytes_to_plain(self, bytes):
         plain = ""
-        for baitas in baitai:
-            plain += chr(baitas)
+        for byte in bytes:
+            plain += chr(byte)
         return plain
     
-class lab3(QWidget):
+class MainWindow(QWidget):
     def __init__(self, win_parrent = None):
-        super(lab3, self).__init__()
+        super(MainWindow, self).__init__()
         self.GUI()
-        self.connect(self.atidaryti_teksta, SIGNAL('clicked()'), self.atidaryti_faila)
-        self.connect(self.uzk_uzkuoduoti_PushButton, SIGNAL('clicked()'), self.uzkuoduoti_teksta)
-        self.connect(self.dek_uzkuoduoti_PushButton, SIGNAL('clicked()'), self.dekuoduoti_teksta)
-        self.connect(self.baigti_PushButton, SIGNAL('clicked()'), self.baigti_darba)
+        self.connect(self.open_file_PushButton, SIGNAL('clicked()'), self.open_file)
+        self.connect(self.encrypt_PushButton, SIGNAL('clicked()'), self.encrypt_text)
+        self.connect(self.dencrypt_PushButton, SIGNAL('clicked()'), self.decrypt_text)
+        self.connect(self.quit_PushButton, SIGNAL('clicked()'), self.quit)
 
-    def atidaryti_faila(self):
-        failo_pav = QFileDialog.getOpenFileName(self, 'Atidaryti faila', '.')
-        self.failo_pavadinimas_Label.setText(failo_pav)
-        f_atidaryti = open(failo_pav) 
-        tekstas = f_atidaryti.read()
-        self.uzk_text_plain_QPlainTextEdit.setPlainText(tekstas)
-        f_atidaryti.close()
+    def open_file(self):
+        file_name = QFileDialog.getOpenFileName(self, 'Open File', '.')
+        self.file_name_Label.setText(file_name)
+        f_open = open(file_name) 
+        text = f_open.read()
+        self.encrypt_text_plain_PlainTextEdit.setPlainText(text)
+        f_open.close()
     
-    def uzkuoduoti_teksta(self):
-        baitai = self.uzk_bitai_LineEdit.displayText()
-        tekstas = self.uzk_text_plain_QPlainTextEdit.toPlainText()
-        raktas = self.uzk_raktas_LineEdit.displayText()
-        if tekstas == "" or baitai == "" or int(baitai) > 256 or int(baitai) < 1 or raktas == "":
-            self.klaida()
+    def encrypt_text(self):
+        bits = self.encrypt_bits_LineEdit.displayText()
+        text = self.encrypt_text_plain_PlainTextEdit.toPlainText()
+        key = self.encrypt_key_LineEdit.displayText()
+        if text == "" or bits == "" or int(bits) > 256 or int(bits) < 1 or key == "":
+            self.error()
         else:
             rc4 = RC4_CBC()
             start_time = time.time()
-            uzkuoduotas_tekstas = rc4.encRC4_CBC(str(tekstas), int(baitai), str(raktas))
+            encrypted_text = rc4.encRC4_CBC(str(text), int(bits), str(key))
             elapsed_time = time.time() - start_time
-            self.uzk_laikas_tikras_Label.setText("%.3f" % elapsed_time)
-            self.uzk_text_crypt_QPlainTextEdit.setPlainText(str(uzkuoduotas_tekstas))
+            self.encrypt_time_seconds_Label.setText("%.3f" % elapsed_time)
+            self.encrypt_text_crypt_PlainTextEdit.setPlainText(str(encrypted_text))
             
             
-    def dekuoduoti_teksta(self):
-        baitai = self.dek_bitai_LineEdit.displayText()
-        tekstas = self.dek_text_plain_QPlainTextEdit.toPlainText()
-        raktas = self.dek_raktas_LineEdit.displayText()
-        if tekstas == "" or baitai == "" or int(baitai) > 256 or int(baitai) < 1 or raktas == "":
-            self.klaida()
+    def decrypt_text(self):
+        bits = self.dencrypt_bits_LineEdit.displayText()
+        text = self.dencrypt_text_plain_PlainTextEdit.toPlainText()
+        key = self.dencrypt_key_LineEdit.displayText()
+        if text == "" or bits == "" or int(bits) > 256 or int(bits) < 1 or key == "":
+            self.error()
         else:
             rc4 = RC4_CBC()
             start_time = time.time()
-            dekuoduotas_tekstas = rc4.decRC4_CBC(str(tekstas), int(baitai), str(raktas))
+            dencrypted_text = rc4.decRC4_CBC(str(text), int(bits), str(key))
             elapsed_time = time.time() - start_time
-            self.dek_laikas_tikras_Label.setText("%.3f" % elapsed_time)
-            self.dek_text_crypt_QPlainTextEdit.setPlainText(str(dekuoduotas_tekstas))
+            self.dencrypt_time_seconds_Label.setText("%.3f" % elapsed_time)
+            self.dencrypt_text_crypt_PlainTextEdit.setPlainText(str(dencrypted_text))
             
-    def klaida(self):
-        QMessageBox.critical(self, "Klaida!" , "Neteisingai ivesti parametrai!", QMessageBox.Ok)
+    def error(self):
+            QMessageBox.critical(self, "Error", "Something has gone terribly terribly wrong! :(")
     
-    def baigti_darba(self):
+    def quit(self):
         sys.exit()
         
     def GUI(self):
-        self.atidaryti_teksta = QPushButton("Atidaryti teksta")
-        self.atidaryti_teksta.setFixedWidth(120)
-        self.failo_pavadinimas_Label = QLabel("(failas)")
+        self.open_file_PushButton = QPushButton("Open File")
+        self.open_file_PushButton.setFixedWidth(120)
+        self.file_name_Label = QLabel("(File Name)")
         
-        self.uzk_uzkuoduoti_PushButton = QPushButton("Uzkuoduoti")
-        self.uzk_raktas_Label = QLabel("Raktas: ")
-        self.uzk_raktas_LineEdit = QLineEdit()
-        self.uzk_bitai_Label = QLabel("Bitai: ")
-        self.uzk_bitai_LineEdit = QLineEdit()
-        self.uzk_laikas_Label = QLabel("Laikas uzk. sek.: ")
-        self.uzk_laikas_tikras_Label = QLabel("(laikas)")
-        self.uzk_text_plain_QPlainTextEdit = QPlainTextEdit()
-        self.uzk_text_crypt_QPlainTextEdit = QPlainTextEdit()
-        self.uzk_text_crypt_QPlainTextEdit.setReadOnly(True)
+        self.encrypt_PushButton = QPushButton("Encrypt")
+        self.encrypt_key_Label = QLabel("Key: ")
+        self.encrypt_key_LineEdit = QLineEdit()
+        self.encrypt_bits_Label = QLabel("Bits: ")
+        self.encrypt_bits_LineEdit = QLineEdit()
+        self.encrypt_time_Label = QLabel("Encryption time: ")
+        self.encrypt_time_seconds_Label = QLabel("(sec)")
+        self.encrypt_text_plain_PlainTextEdit = QPlainTextEdit()
+        self.encrypt_text_crypt_PlainTextEdit = QPlainTextEdit()
+        self.encrypt_text_crypt_PlainTextEdit.setReadOnly(True)
         
-        self.dek_uzkuoduoti_PushButton = QPushButton("Dekuoduoti")
-        self.dek_raktas_Label = QLabel("Raktas: ")
-        self.dek_raktas_LineEdit = QLineEdit()
-        self.dek_bitai_Label = QLabel("Bitai: ")
-        self.dek_bitai_LineEdit = QLineEdit()
-        self.dek_laikas_Label = QLabel("Laikas dek. sek.: ")
-        self.dek_laikas_tikras_Label = QLabel("(laikas)")
-        self.dek_text_plain_QPlainTextEdit = QPlainTextEdit()
-        self.dek_text_crypt_QPlainTextEdit = QPlainTextEdit()
-        self.dek_text_crypt_QPlainTextEdit.setReadOnly(True)
+        self.dencrypt_PushButton = QPushButton("Decrypt")
+        self.dencrypt_key_Label = QLabel("Key: ")
+        self.dencrypt_key_LineEdit = QLineEdit()
+        self.dencrypt_bits_Label = QLabel("Bits: ")
+        self.dencrypt_bits_LineEdit = QLineEdit()
+        self.dencrypt_time_Label = QLabel("Decryption time: ")
+        self.dencrypt_time_seconds_Label = QLabel("(sec)")
+        self.dencrypt_text_plain_PlainTextEdit = QPlainTextEdit()
+        self.dencrypt_text_crypt_PlainTextEdit = QPlainTextEdit()
+        self.dencrypt_text_crypt_PlainTextEdit.setReadOnly(True)
         
-        self.baigti_PushButton =  QPushButton("Baigti darba")    
+        self.quit_PushButton =  QPushButton("Quit")    
         
-        atidaryti_faila_Grid = QGridLayout()
-        atidaryti_faila_Grid.addWidget(self.atidaryti_teksta, 1, 0)
-        atidaryti_faila_Grid.addWidget(self.failo_pavadinimas_Label, 1, 1)
+        open_file_grid = QGridLayout()
+        open_file_grid.addWidget(self.open_file_PushButton, 1, 0)
+        open_file_grid.addWidget(self.file_name_Label, 1, 1)
         
-        uzk_Grid = QGridLayout()
-        uzk_Grid.addWidget(self.uzk_uzkuoduoti_PushButton, 1, 0)
-        uzk_Grid.addWidget(self.uzk_raktas_Label, 1, 1)
-        uzk_Grid.addWidget(self.uzk_raktas_LineEdit, 1, 2)
-        uzk_Grid.addWidget(self.uzk_bitai_Label, 1, 3)
-        uzk_Grid.addWidget(self.uzk_bitai_LineEdit, 1, 4)
-        uzk_Grid.addWidget(self.uzk_laikas_Label, 1, 5)
-        uzk_Grid.addWidget(self.uzk_laikas_tikras_Label, 1, 6)
-        uzk_tekstai_Grid = QGridLayout()
-        uzk_tekstai_Grid.addWidget(self.uzk_text_plain_QPlainTextEdit, 1, 0)
-        uzk_tekstai_Grid.addWidget(self.uzk_text_crypt_QPlainTextEdit, 1, 1)
+        encrypt_options_GridLayout = QGridLayout()
+        encrypt_options_GridLayout.addWidget(self.encrypt_PushButton, 1, 0)
+        encrypt_options_GridLayout.addWidget(self.encrypt_key_Label, 1, 1)
+        encrypt_options_GridLayout.addWidget(self.encrypt_key_LineEdit, 1, 2)
+        encrypt_options_GridLayout.addWidget(self.encrypt_bits_Label, 1, 3)
+        encrypt_options_GridLayout.addWidget(self.encrypt_bits_LineEdit, 1, 4)
+        encrypt_options_GridLayout.addWidget(self.encrypt_time_Label, 1, 5)
+        encrypt_options_GridLayout.addWidget(self.encrypt_time_seconds_Label, 1, 6)
+
+        ecrypt_text_GridLayout = QGridLayout()
+        ecrypt_text_GridLayout.addWidget(self.encrypt_text_plain_PlainTextEdit, 1, 0)
+        ecrypt_text_GridLayout.addWidget(self.encrypt_text_crypt_PlainTextEdit, 1, 1)
         
-        dek_Grid = QGridLayout()
-        dek_Grid.addWidget(self.dek_uzkuoduoti_PushButton, 1, 0)
-        dek_Grid.addWidget(self.dek_raktas_Label, 1, 1)
-        dek_Grid.addWidget(self.dek_raktas_LineEdit, 1, 2)
-        dek_Grid.addWidget(self.dek_bitai_Label, 1, 3)
-        dek_Grid.addWidget(self.dek_bitai_LineEdit, 1, 4)
-        dek_Grid.addWidget(self.dek_laikas_Label, 1, 5)
-        dek_Grid.addWidget(self.dek_laikas_tikras_Label, 1, 6)
-        dek_tekstai_Grid = QGridLayout()
-        dek_tekstai_Grid.addWidget(self.dek_text_plain_QPlainTextEdit, 1, 0)
-        dek_tekstai_Grid.addWidget(self.dek_text_crypt_QPlainTextEdit, 1, 1)
+        decrypt_options_GridLayout = QGridLayout()
+        decrypt_options_GridLayout.addWidget(self.dencrypt_PushButton, 1, 0)
+        decrypt_options_GridLayout.addWidget(self.dencrypt_key_Label, 1, 1)
+        decrypt_options_GridLayout.addWidget(self.dencrypt_key_LineEdit, 1, 2)
+        decrypt_options_GridLayout.addWidget(self.dencrypt_bits_Label, 1, 3)
+        decrypt_options_GridLayout.addWidget(self.dencrypt_bits_LineEdit, 1, 4)
+        decrypt_options_GridLayout.addWidget(self.dencrypt_time_Label, 1, 5)
+        decrypt_options_GridLayout.addWidget(self.dencrypt_time_seconds_Label, 1, 6)
+
+        decrypt_text_GridLayout = QGridLayout()
+        decrypt_text_GridLayout.addWidget(self.dencrypt_text_plain_PlainTextEdit, 1, 0)
+        decrypt_text_GridLayout.addWidget(self.dencrypt_text_crypt_PlainTextEdit, 1, 1)
         
         self.vbox = QVBoxLayout()
-        self.vbox.addLayout(atidaryti_faila_Grid)
-        self.vbox.addLayout(uzk_Grid)
-        self.vbox.addLayout(uzk_tekstai_Grid)
-        self.vbox.addLayout(dek_Grid)
-        self.vbox.addLayout(dek_tekstai_Grid)
-        self.vbox.addWidget(self.baigti_PushButton)
+        self.vbox.addLayout(open_file_grid)
+        self.vbox.addLayout(encrypt_options_GridLayout)
+        self.vbox.addLayout(ecrypt_text_GridLayout)
+        self.vbox.addLayout(decrypt_options_GridLayout)
+        self.vbox.addLayout(decrypt_text_GridLayout)
+        self.vbox.addWidget(self.quit_PushButton)
         
         self.setLayout(self.vbox)
-        self.setWindowTitle("Lab 3 - RC4 - CBC")
+        self.setWindowTitle("RC4 CBC mode de-encrypter")
         self.setFixedSize(900, 700)    
 
 if __name__ == "__main__":
     prog  = QApplication(sys.argv)
-    lab = lab3()
-    lab.show()
+    main_window = MainWindow()
+    main_window.show()
     sys.exit(prog.exec_())
